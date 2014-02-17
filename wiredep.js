37b9850088a9ecbb86e2cefb6232ac4e8707469a
Stephen Sawchuk <sawchuk@gmail.com>
2014-02-17 16:10:50 -0500

    support through/streams/things.

--- wiredep.js
+++ wiredep.js
@@ -8,20 +8,23 @@
 
 'use strict';
 
+var fs = require('fs');
 var glob = require('glob');
 var helpers = require('./lib/helpers');
+var path = require('path');
+var through = require('through2');
 
 /**
  * Wire up the html files with the Bower packages.
  *
  * @param  {object} config  the global configuration object
  */
-module.exports = function (opts) {
+var wiredep = function (opts) {
   var config = helpers.createStore();
 
   config.set
-    ('bower.json', opts.bowerJson)
-    ('bower-directory', opts.directory)
+    ('bower.json', opts.bowerJson || JSON.parse(fs.readFileSync('./bower.json')))
+    ('bower-directory', opts.directory || 'bower_components')
     ('dependencies', opts.dependencies === false ? false : true)
     ('dev-dependencies', opts.devDependencies)
     ('exclude', opts.exclude)
@@ -29,12 +32,15 @@ module.exports = function (opts) {
     ('global-dependencies', helpers.createStore())
     ('ignore-path', opts.ignorePath)
     ('src', [])
+    ('stream', opts.stream ? opts.stream : {})
     ('warnings', []);
 
-  (Array.isArray(opts.src) ? opts.src : [opts.src]).
-    forEach(function (pattern) {
-      config.set('src', config.get('src').concat(glob.sync(pattern)));
-    });
+  if (!opts.stream) {
+    (Array.isArray(opts.src) ? opts.src : [opts.src]).
+      forEach(function (pattern) {
+        config.set('src', config.get('src').concat(glob.sync(pattern)));
+      });
+  }
 
   require('./lib/detect-dependencies')(config);
   require('./lib/inject-dependencies')(config);
@@ -42,4 +48,38 @@ module.exports = function (opts) {
   if (config.get('warnings')) {
     helpers.warn(config.get('warnings'));
   }
+
+  return config.get('stream').src || config.get('src');
 };
+
+wiredep.stream = function (opts) {
+  opts = opts || {};
+
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+
+    if (file.isStream()) {
+      this.emit('error', 'Streaming not supported');
+      return cb();
+    }
+
+    try {
+      opts.stream = {
+        src: file.contents.toString(),
+        fileType: path.extname(file.path).substr(1)
+      };
+
+      file.contents = new Buffer(wiredep(opts));
+    } catch (err) {
+      this.emit('error', err);
+    }
+
+    this.push(file);
+    cb();
+  });
+};
+
+module.exports = wiredep;
