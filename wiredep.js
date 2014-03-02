ce0e162dd9e81e348500928a2478df9a20dcc4f7
jdichev <jordan@dichev.net>
2014-03-02 23:36:00 +0200

    Enable component specific distDirs configuration

--- wiredep.js
+++ wiredep.js
@@ -33,7 +33,8 @@ var wiredep = function (opts) {
     ('ignore-path', opts.ignorePath)
     ('src', [])
     ('stream', opts.stream ? opts.stream : {})
-    ('warnings', []);
+    ('warnings', [])
+    ('distDirs', opts.distDirs || ['dist']);
 
   if (!opts.stream) {
     (Array.isArray(opts.src) ? opts.src : [opts.src]).
