9c5174fdc321d3cc2aaf41fe0656c3f50437911a
Ruy Adorno <ruyadorno@hotmail.com>
2014-08-25 11:28:36 -0400

    Moved wiredep.stream method to its own module

--- wiredep.js
+++ wiredep.js
@@ -110,36 +110,6 @@ function findBowerDirectory(cwd) {
   return directory;
 }
 
-wiredep.stream = function (opts) {
-  opts = opts || {};
-
-  return $.through2.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-      this.push(file);
-      return cb();
-    }
-
-    if (file.isStream()) {
-      this.emit('error', 'Streaming not supported');
-      return cb();
-    }
-
-    try {
-      opts.stream = {
-        src: file.contents.toString(),
-        path: file.path,
-        fileType: $.path.extname(file.path).substr(1)
-      };
-
-      file.contents = new Buffer(wiredep(opts));
-    } catch (err) {
-      this.emit('error', err);
-    }
-
-    this.push(file);
-    cb();
-  });
-};
-
+wiredep.stream = require('./lib/wiredep-stream');
 
 module.exports = wiredep;
