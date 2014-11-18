ca158f9147dbbc9e38608e0a9be2787a602d1d64
David Rogers <al-the-x@users.noreply.github.com>
2014-11-18 17:47:52 -0500

    Create index-actual.js

---
+++ test/fixture/js/index-actual.js
@@ -0,0 +1,11 @@
+// Like you would expect your `karma.conf.js` file to look...
+module.exports = function(config){
+  config.files = [
+    // These are the most important...
+    // bower:js
+    // endbower
+    
+    // But just in case...
+    // bower:css
+    // endbower
+};
