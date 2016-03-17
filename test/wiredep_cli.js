366e6555f960e4f76b63fb688a179b5c2216a8a6
Eddie Monge <eddie@eddiemonge.com>
2016-03-16 17:43:08 -0700

    expanded wiredep_cli testing

--- test/wiredep_cli.js
+++ test/wiredep_cli.js
@@ -1,54 +1,87 @@
 /*jshint latedef:false */
-/*global after, describe, it, before */
+/*global after, describe, it, before, after */
 
 'use strict';
 
 var fs = require('fs-extra');
-var path = require('path');
+var resolve = require('path').resolve;
 var assert = require('chai').assert;
+var sinon = require('sinon');
 
 describe('wiredep-cli', function () {
-  before(function() {
+  before(function () {
     fs.copySync('test/fixture', '.tmp');
     process.chdir('.tmp');
-
-    // this is required to prevent require from caching the CLI script
-    delete require.cache[require.resolve('../wiredep-cli')];
   });
+
   after(function () {
     process.chdir('..');
     fs.removeSync('.tmp');
   });
 
-  describe('replace functionality', function () {
-    function testReplaceCli(args, filePaths) {
+  function reset () {
+    delete require.cache[require.resolve('../wiredep-cli')];
+  }
 
-      process.argv = args;
+  describe('replace functionality', function () {
+    function runCli (arg) {
+      var src = getFilePaths('index', 'html');
+
+      reset();
+      process.argv = ['', '', arg[0], src.actual, arg[1], 'bower.json'];
       require('../wiredep-cli');
 
       assert.deepEqual(
-        filePaths.read('expected').split('\n'),
-        filePaths.read('actual').split('\n')
+        src.read('expected').split('\n'),
+        src.read('actual').split('\n')
       );
     }
 
-    it('should work with html and extra parameters, long notation', function() {
-      var filePaths = getFilePaths('index', 'html');
-      testReplaceCli(['foo', 'bar', '--src', filePaths.actual, '--bowerJson', 'bower.json'], filePaths);
+    it('should work with long notation', runCli.bind(this,
+      ['--src', '--bowerJson'])
+    );
+
+    it('should work with short notation', runCli.bind(this, ['-s', '-b']));
+  });
+
+  describe('argument checks', function () {
+    function runLog (args, errMsg) {
+      var stub = sinon.stub(console, 'log');
+
+      reset();
+      process.argv = ['', ''].concat(args);
+      require('../wiredep-cli');
+
+      assert(stub.calledOnce, 'Console.log was not called');
+      assert(stub.calledWithMatch(errMsg), 'Message did not match');
+      stub.restore();
+    }
+
+    it('should return the version', function () {
+      var version = require('../package.json').version;
+      runLog(['-v'], version);
+      runLog(['--version'], version);
     });
 
-    it('should work with html and extra parameters, short notation', function() {
-      var filePaths = getFilePaths('index', 'html');
-      testReplaceCli(['foo', 'bar', '-s', filePaths.actual, '-b', 'bower.json'], filePaths);
+    it('should display help when called with no args or help ones', function () {
+      [[], ['-h'], ['--help']].forEach(function (arg) {
+        runLog(arg, /^Wire Bower dependencies to your source code/);
+      });
+    });
+
+    it('should log when source is not specified', function () {
+      var msg = /Source file not specified/;
+      runLog(['-b', 'bower.json'], msg);
+      runLog(['--bowerJson', 'bower.json'], msg);
     });
   });
 });
 
-function getFilePaths(fileName, fileType) {
+function getFilePaths (fileName, fileType) {
   var extension = fileType.match(/([^/]*)[/]*/)[1];
   var filePaths = {
-    expected: path.resolve(fileType, fileName + '-expected.' + extension),
-    actual: path.resolve(fileType, fileName + '-actual.' + extension),
+    expected: resolve(fileType, fileName + '-expected.' + extension),
+    actual: resolve(fileType, fileName + '-actual.' + extension),
     read: function (type) {
       return fs.readFileSync(filePaths[type], { encoding: 'utf8' });
     }
